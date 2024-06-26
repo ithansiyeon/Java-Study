f0c18d4fb8edb3db3f6044d2081cca8df24b1e6d
itsiyeon <itsiyeon@nate.com>
2024-06-26 22:22:37 +0900

    객체지향 - 문제와 풀이

---
+++ java-basic/src/oop1/Rectangle.java
@@ -0,0 +1,18 @@
+package oop1;
+
+public class Rectangle {
+    int width;
+    int height;
+
+    public int calculateArea() {
+        return width * height;
+    }
+
+    public int calculatePerimeter() {
+        return 2 * (width + height);
+    }
+
+    public boolean isSquare() {
+        return width == height;
+    }
+}
