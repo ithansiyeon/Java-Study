e3a3087e837d4f7d7861059dda50d0c585987da4
itsiyeon <itsiyeon@nate.com>
2024-08-20 21:38:38 +0900

    상속과 기능 추가

--- java-basic/src/extends1/ex1/ElectricCar.java
+++ java-basic/src/extends1/ex1/ElectricCar.java
@@ -1,6 +1,8 @@
 package extends1.ex1;
 
-public class ElectricCar {
+import extends1.ex3.Car;
+
+public class ElectricCar extends Car {
     public void move() {
         System.out.println("차를 이동합니다.");
     }
--- java-basic/src/extends1/ex1/GasCar.java
+++ java-basic/src/extends1/ex1/GasCar.java
@@ -1,6 +1,8 @@
 package extends1.ex1;
 
-public class GasCar {
+import extends1.ex3.Car;
+
+public class GasCar extends Car {
     public void move() {
         System.out.println("차를 이동합니다.");
     }
