a95144d341a4a9c81759855d0f96924905b30255
itsiyeon <itsiyeon@nate.com>
2024-10-09 22:42:59 +0900

    다형성 문제와 풀이

---
+++ java-basic/src/poly/ex/pay1/PayMain2.java
@@ -0,0 +1,25 @@
+package poly.ex.pay1;
+
+import java.util.Scanner;
+
+public class PayMain2 {
+	public static void main(String[] args) {
+		PayService payService = new PayService();
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			System.out.print("결제 수단을 입력하세요: ");
+			String payOption = scanner.nextLine();
+
+			if (payOption.equals("exit")) {
+				System.out.println("프로그램을 종료합니다.");
+				return;
+			}
+
+			System.out.print("결재 금액을 입력하세요: ");
+			int amount = scanner.nextInt();
+			scanner.nextLine(); //\n
+			payService.processPay(payOption, amount);
+		}
+	}
+}
