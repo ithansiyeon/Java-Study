b4242a128b7308a400dfae88726ed687df8a0c13
itsiyeon <itsiyeon@nate.com>
2024-10-09 22:36:52 +0900

    다형성 문제와 풀이

--- java-basic/src/poly/ex/pay1/PayService.java
+++ java-basic/src/poly/ex/pay1/PayService.java
@@ -2,12 +2,11 @@ package poly.ex.pay1;
 
 public class PayService {
 	public void processPay(String option, int amount) {
-		boolean result = false;
 		System.out.println("결제를 시작합니다: option=" + option + ", amount=" + amount);
 		// 결재 수단 선택
 		Pay pay = PayStore.findPay(option);
 		// 결재를 실제로 실행
-		result = pay.pay(amount);
+		boolean result = pay.pay(amount);
 
 		if (result) {
 			System.out.println("결제가 성공했습니다.");
