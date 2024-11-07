726e164d42eda6905493b4186173e5533c35caf9
itsiyeon <itsiyeon@nate.com>
2024-11-07 22:18:58 +0900

    java.lang 패키지 소개

---
+++ java-mid1/src/lang/LangMain.java
@@ -0,0 +1,14 @@
+package lang;
+
+public class LangMain {
+
+	// java.lang
+	// Object
+	// String
+	// Integer, Long, Double 래퍼 타입
+	// Class
+	// System
+	public static void main(String[] args) {
+		System.out.println("hello java");
+	}
+}
