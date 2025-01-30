edd20042c4c9dfed9d70f2fd8f2fb3997c67f8db
itsiyeon <itsiyeon@nate.com>
2025-01-30 16:22:17 +0900

    열거형-리팩토링3

---
+++ java-mid1/src/enumration/ref3/DiscountService.java
@@ -0,0 +1,7 @@
+package enumration.ref3;
+
+public class DiscountService {
+	public int discount(Grade grade, int price) {
+		return grade.discount(price);
+	}
+}
--- java-mid1/src/enumration/ref3/EnumRefMain3_1.java
+++ java-mid1/src/enumration/ref3/EnumRefMain3_1.java
@@ -1,7 +1,7 @@
-package enumration.ref2;
+package enumration.ref3;
 
 
-public class EnumRefMain2 {
+public class EnumRefMain3_1 {
 	public static void main(String[] args) {
 		int price = 10000;
 		DiscountService discountService = new DiscountService();
