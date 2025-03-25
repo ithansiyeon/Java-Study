d0c8e470849fad67c1ba2c3ded243239f890d36d
itsiyeon <itsiyeon@nate.com>
2025-03-25 12:11:12 +0900

    내부 클래스의 활용

---
+++ java-mid1/src/nested/inner/ex2/CarMain.java
@@ -0,0 +1,10 @@
+package nested.inner.ex2;
+
+import nested.inner.ex1.Engine;
+
+public class CarMain {
+	public static void main(String[] args) {
+		Car myCar = new Car("Model Y", 100);
+		myCar.start();
+	}
+}
