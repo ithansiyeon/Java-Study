202e417ecc88594df3bbf23000d1c9f03e13ea42
itsiyeon <itsiyeon@nate.com>
2025-04-27 15:20:02 +0900

    익명 클래스

---
+++ java-mid1/src/nested/anonymous/ex/Ex0RefMain.java
@@ -0,0 +1,16 @@
+package nested.anonymous.ex;
+
+import nested.local.Printer;
+
+public class Ex0RefMain {
+	public static void hello(String str) {
+		System.out.println("프로그램 시작");
+		System.out.println("Hello " + str);
+		System.out.println("프로그램 종료");
+	}
+
+	public static void main(String[] args) {
+		hello("Java");
+		hello("Spring");
+	}
+}
