6fad10d3d2405d54a93d11b272b8288a5f0f971c
itsiyeon <itsiyeon@nate.com>
2025-06-03 20:13:43 +0900

    try-with-resources

--- java-mid1/src/exception/ex4/MainV4.java
+++ java-mid1/src/exception/ex4/MainV4.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class MainV4 {
 	public static void main(String[] args) {
-		NetworkServiceV4 networkService = new NetworkServiceV4();
+		NetworkServiceV5 networkService = new NetworkServiceV5();
 		Scanner scanner = new Scanner(System.in);
 		while (true) {
 			System.out.print("전송할 문자: ");
