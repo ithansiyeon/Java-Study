2ceabe57a51df547cd901230f2fd4fa4bb54cf30
itsiyeon <itsiyeon@nate.com>
2025-08-20 22:44:00 +0900

    13. 타입 매개변수 제한3 - 제네릭 도입과 실패

---
+++ java-mid2/src/generic/ex3/AnimalHospitalMainV2.java
@@ -0,0 +1,12 @@
+package generic.ex3;
+
+import generic.animal.Dog;
+
+public class AnimalHospitalMainV2 {
+	public static void main(String[] args) {
+		AnimalHospitalV2<Dog> dogHospital = new AnimalHospitalV2<>();
+		AnimalHospitalV2<Dog> catHospital = new AnimalHospitalV2<>();
+		AnimalHospitalV2<Integer> integerHospital = new AnimalHospitalV2<>();
+		AnimalHospitalV2<Object> objectHospital = new AnimalHospitalV2<>();
+	}
+}
