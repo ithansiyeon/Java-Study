2e008ece06cf2ec86374eec5f21ccd67525fdfc2
itsiyeon <itsiyeon@nate.com>
2025-09-28 16:45:35 +0900

    문제와 풀이2

---
+++ java-mid2/src/generic/test/ex3/ShuttleTest.java
@@ -0,0 +1,24 @@
+package generic.test.ex3;
+
+import generic.test.ex3.unit.Marine;
+import generic.test.ex3.unit.Zealot;
+import generic.test.ex3.unit.Zeraling;
+
+public class ShuttleTest {
+	public static void main(String[] args) {
+		Shuttle<Marine> shuttle1 = new Shuttle<>();
+		shuttle1.in(new Marine("마린", 40));
+		shuttle1.showInfo();
+
+		Shuttle<Zeraling> shuttle2 = new Shuttle<>();
+		shuttle2.in(new Zeraling("저글링", 35));
+		shuttle2.showInfo();
+
+		Shuttle<Zealot> shuttle3 = new Shuttle<>();
+		shuttle3.in(new Zealot("질럿", 100));
+		shuttle3.showInfo();
+
+		UnitPrinter.printV1(shuttle1);
+		UnitPrinter.printV2(shuttle1);
+	}
+}
---
+++ java-mid2/src/generic/test/ex3/unit/Zeraling.java
@@ -0,0 +1,8 @@
+package generic.test.ex3.unit;
+
+public class Zeraling extends BioUnit{
+
+	public Zeraling(String name, int hp) {
+		super(name, hp);
+	}
+}
